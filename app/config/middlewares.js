7429b2dfd0c2e46da6cfd1971b3778d0227a6229
Eduardo Pinheiro <3dup1n@gmail.com>
2020-10-01 19:11:48 -0300

    cors added to test other apps

--- app/config/middlewares.js
+++ app/config/middlewares.js
@@ -1,7 +1,9 @@
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 module.exports = (app) => {
   app.use(bodyParser.json());
+  app.use(cors());
   app.use(
     bodyParser.urlencoded({
       extended: true,
